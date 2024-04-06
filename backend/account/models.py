c254edd01d5c07ecf51619e0dddb8e654a329eba
dt00561 <dt00561@surrey.ac.uk>
2024-04-06 15:39:22 +0100

    app,url,serializers,api and models for chat

--- backend/account/models.py
+++ backend/account/models.py
@@ -50,3 +50,21 @@ class User(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = 'email'
     EMAIL_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+
+class ConnectionRequest(models.Model):
+    SENT = 'sent'
+    ACCEPTED = 'accepted'
+    REJECTED = 'rejected'
+
+    STATUS_CHOICES = (
+        (SENT, 'Sent'),
+        (ACCEPTED, 'Accepted'),
+        (REJECTED, 'Rejected'),
+    )
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_for = models.ForeignKey(User, related_name='received_connectionrequests', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, related_name='created_connectionrequests', on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=SENT)
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('api/', include('account.urls')),
+    path('api/chat/', include('chat.urls')),
     path('admin/', admin.site.urls),
 ]
