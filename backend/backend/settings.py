0f01c0cba2edad580b4adf35766666c03b44cc1c
mr01562 <mr01562@surrey.ac.uk>
2024-05-12 20:12:45 +0100

    notifications

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -77,6 +77,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'account',
+    'notifications',
     'rest_framework',
     'rest_framework_simplejwt',
     'corsheaders',
