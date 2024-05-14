410fd9d8d05756a0901b5fa8cae9a199c8953898
Tejas <tejasa7tiarf@gmail.com>
2024-05-14 14:33:24 +0100

    Re-entering working code into the repository

--- backend/account/api.py
+++ backend/account/api.py
@@ -16,7 +16,7 @@ def me(request):
         'id': request.user.id,
         'name': request.user.name,
         'email': request.user.email,
-        'avatar': request.user.avatar.url,
+        'avatar': request.user.get_avatar()
     })
 
 @api_view(['POST'])
